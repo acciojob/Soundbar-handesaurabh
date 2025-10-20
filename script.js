6fda7cb13d5dadd52e3f60f3d8fd569a90dd1e0e
handesaurabh <handesaurabh2004@gmail.com>
2025-10-20 19:27:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 //your JS code here. If required.
   const buttons = document.querySelectorAll(".btn");
-    const stopButton = document.querySelector(".stop");
-    let currentAudio = null;
- buttons.forEach(button => {
-      button.addEventListener('click', () => {
-        const soundName = button.dataset.sound;
-        if(currentAudio) {
-          currentAudio.pause();
-          currentAudio.currentTime = 0;
-        }
-        currentAudio = new Audio(`sounds/${soundName}.mp3`);
-        currentAudio.play();
-      });
-    });
- stopButton.addEventListener('click', () => {
-      if(currentAudio) {
-        currentAudio.pause();
-        currentAudio.currentTime = 0;
-      }
-    });
+const stopButton = document.querySelector(".stop");
+let currentAudio = null;
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const soundName = button.dataset.sound;
+    
+    if(currentAudio) {
+      currentAudio.pause();
+      currentAudio.remove();
+    }
+    
+    currentAudio = document.createElement('audio');
+    currentAudio.src = `sounds/${soundName}.mp3`;
+    
+    document.body.appendChild(currentAudio);
+    
+    currentAudio.play();
+  });
+});
+
+stopButton.addEventListener('click', () => {
+  if(currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
